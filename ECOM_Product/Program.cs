8d42bcffa9724e4bda506f66e184ee90ddb28634
Zhengming <zhengming678@gmail.com>
2024-06-12 21:09:56 -0400

    New product class added

---
+++ ECOM_Product/Program.cs
@@ -0,0 +1,19 @@
+﻿using ECOM_Product;
+
+class Program
+{
+
+    public static void main(String[] args)
+    {
+        var productOne = new Product(1, "Utensils", 3.45m, 50);
+
+        Console.WriteLine($"Product: {productOne.ProductName}, Stock: {productOne.Stock}");
+
+        productOne.IncreaseStock(10);
+        Console.WriteLine($"New stock value is: {productOne.Stock}");
+
+        productOne.DecreaseStock(70);
+        Console.WriteLine($"New stock value is: {productOne.Stock}");
+    }
+
+}
